11b8f4667f905185da6168a428073c81c52b20c0
mh <mre.ezboy3@gmail.com>
2017-07-02 19:34:38 +0330

    update

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var telegram = require('telegram-bot-api');
 
 var api = new telegram({
-        token: '<PUT YOUR TOKEN HERE>',
+        token: '376731137:AAEQdPZdUaPhQsL_4X02r5VPmfe2KyGTreE',
         updates: {
         	enabled: true
     }
@@ -10,5 +10,28 @@ var api = new telegram({
 api.on('message', function(message)
 {
     // Received text message
+    // if(message.chat.id == 216029435){
+    //   api.sendMessage({
+    //   	chat_id: 1499005107,
+    //   	text: message.text
+    //   });
+    // }
+    // if(message.chat.id == 1499005107){
+      api.sendMessage({
+      	chat_id: message.chat.id,
+      	text: message.text
+      });
+    // }
     console.log(message);
 });
+
+api.on('inline.result', function(message)
+{
+    // Received chosen inline result
+    console.log(message);
+});
+
+
+//376731137:AAEQdPZdUaPhQsL_4X02r5VPmfe2KyGTreE
+// 216029435
+//1499005107
